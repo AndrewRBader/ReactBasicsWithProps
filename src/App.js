a1485b61a88dc6236c272f9fb4963fb43b3ef18e
RyanMacc <arbader2142@gmail.com>
2022-05-04 16:15:47 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,16 @@
-export default function App() {
-  return <></>
+// bring in React from React
+import React from 'react';
+
+// greet function
+function greet(){
+  return "Hello World"
 }
+
+// define hello function
+const Hello = () =>{
+  return (<h1>{greet()}</h1>);
+}
+
+
+//export the Hello function
+export default Hello;
